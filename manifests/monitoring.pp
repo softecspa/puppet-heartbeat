bee28dc714b4c7be91e3a82d925eef311a3d1d3d
root on puppetmaster <root@puppetmaster.vagrant.local>
2013-12-12 12:07:25 +0100

    corretto check heartbeat_link

--- manifests/monitoring.pp
+++ manifests/monitoring.pp
@@ -1,16 +1,14 @@
 class heartbeat::monitoring {
 
-  nrpe::check { 'heartbeat':
-    contrib => true,
-  }
+  nrpe::check {'heartbeat_link': contrib => true}
 
   @@nagios::check { "heartbeat-${::hostname}":
     host                  => $hostname,
     checkname             => 'check_nrpe_1arg',
-    service_description   => "Heartbeat",
+    service_description   => "Heartbeat link",
     notifications_enabled => 0,
     target                => "heartbeat_${::hostname}.cfg",
-    params                => "!check_heartbeat",
+    params                => "!check_heartbeat_link",
     tag                   => "nagios_check_heartbeat_${heartbeat::nagios_hostname}",
   }
 
