38807ac1064bdcb2ae12bb7aad9568ea3475b115
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-02-03 13:51:09 +0000

    raffinati parametri per monitoraggio

--- manifests/monitoring.pp
+++ manifests/monitoring.pp
@@ -1,14 +1,29 @@
-class heartbeat::monitoring {
+class heartbeat::monitoring (
+  $monitored_hostname     = $hostname,
+  $notifications_enabled  = undef,
+  $notification_period    = undef,
+){
 
   nrpe::check {'heartbeat_link': contrib => true}
 
+  $service_description = $monitored_hostname?{
+    $hostname => 'Heartbeat link',
+    default   => "${hostname} heartbeat link",
+  }
+
+  $nrpe_check = $monitored_hostname?{
+    $hostname => '!check_heartbeat_link',
+    default   => "!check_heartbeat_link_${hostname}",
+  }
+
   @@nagios::check { "heartbeat-${::hostname}":
-    host                  => $hostname,
+    host                  => $monitored_hostname,
     checkname             => 'check_nrpe_1arg',
-    service_description   => "Heartbeat link",
-    notifications_enabled => 0,
+    service_description   => $service_description,
+    notifications_enabled => $notifications_enabled,
+    notification_period   => $notification_period,
     target                => "heartbeat_${::hostname}.cfg",
-    params                => "!check_heartbeat_link",
+    params                => $nrpe_check,
     tag                   => "nagios_check_heartbeat_${heartbeat::nagios_hostname}",
   }
 
